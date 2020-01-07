36426e2f9364478f6040de1fb56fdcf04ac65fb1
Jayy-Lmao <jameswarringtonholman@gmail.com>
2020-01-07 21:09:50 +1100

    style: putting things in their places

--- src/components/main.js
+++ src/components/main.js
@@ -4,14 +4,8 @@ import { jsx } from "@emotion/core";
 import * as styles from "../styles/main";
 
 const Main = () => (
-  <div>
-    <iframe
-      css={styles.videoPlayer}
-      src="https://www.youtube.com/embed/hHW1oY26kxQ?controls=0"
-      frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    />
+  <div css={styles.container}>
+    <iframe css={styles.videoPlayer} src="https://www.youtube.com/embed/hHW1oY26kxQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 );
 
--- src/styles/main.js
+++ src/styles/main.js
@@ -1,7 +1,14 @@
 import {css} from '@emotion/core';
 
+export const container = css`
+position: relative;
+padding-bottom: 56.25%;
+padding-top: 30px; height: 0; overflow: hidden
+`
 export const videoPlayer = css`
-  position: relative;
-  height: 100%;
-  width: 100%;
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 100%;
 `
